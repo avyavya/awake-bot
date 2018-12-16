888c7387cf485e553a93bb735c36e04a6c387e2b
h.abe <l-liroki@users.noreply.github.com>
2018-12-17 03:00:59 +0900

    Initialized

---
+++ README.md
@@ -0,0 +1,8 @@
+## Awake Bot
+
+[https://awake-bot.herokuapp.com/](https://awake-bot.herokuapp.com/)
+
+### Refs
+
+- [https://github.com/heroku/go-getting-started](https://github.com/heroku/go-getting-started)
+- [https://github.com/line/line-bot-sdk-go](https://github.com/line/line-bot-sdk-go)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	bot, err := linebot.New(os.Getenv("LINE_CHANNEL_SECRET"), os.Getenv("LINE_CHANNEL_TOKEN"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.GET("/ping", onPing)
+
+	// Setup HTTP Server for receiving requests from LINE platform
+	router.POST("/message", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				c.Writer.WriteHeader(400)
+			} else {
+				c.Writer.WriteHeader(500)
+			}
+			return
+		}
+
+		for _, event := range events {
+			log.Print(event.Type)
+
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					log.Print(message.Text)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+
+	router.Run(":" + port)
+}
+
+func onPing(c *gin.Context) {
+	c.String(http.StatusOK, "Pong.")
+}
